c08e45978a6ff6e5c420e4a10981b57e1d4c0624
FadiBibawy <eng.fadimichel@gmail.com>
2023-02-11 08:18:51 +0100

    some front-end updates

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,10 @@
 // Entry point for the build script in your package.json
+
+//= require jquery
+//= require bootstrap-sprockets
+//= require flatpickr
+
+
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
