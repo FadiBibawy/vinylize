7bcf64c4801df1fdb5efee9e86cdb80f0eafed1f
Amin Ahcene <amin.ahcene@gmail.com>
2023-02-18 14:32:48 +0100

    new font

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( application.scss )
+
